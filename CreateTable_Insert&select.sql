df7ac99d9510d23f46e142e79c228f86042dfee6
Umair Saad <121795621+httpsumairsaad1@users.noreply.github.com>
2024-05-12 18:02:49 +0500

    Create CreateTable_Insert&select.sql

---
+++ CreateTable_Insert&select.sql
@@ -0,0 +1,35 @@
+DECLARE
+  sql_statement VARCHAR2(1000);
+BEGIN
+  
+  sql_statement := 'CREATE TABLE CUSTOMERS(
+                       ID   INT NOT NULL, 
+                       NAME VARCHAR(20) NOT NULL, 
+                       AGE INT NOT NULL, 
+                       ADDRESS VARCHAR(50), 
+                       SALARY DECIMAL(18, 2),
+                       PRIMARY KEY (ID)
+                    )';
+
+  EXECUTE IMMEDIATE sql_statement;
+
+  DBMS_OUTPUT.PUT_LINE('Table CUSTOMERS created successfully.');
+END;
+/
+
+DECLARE
+BEGIN
+  INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY) 
+  VALUES (1, 'Ramesh', 32, 'Ahmedabad', 2000.00);
+
+  DBMS_OUTPUT.PUT_LINE('Row inserted successfully.');
+END;
+/
+
+DECLARE
+BEGIN
+  FOR customer IN (SELECT * FROM CUSTOMERS) LOOP
+    DBMS_OUTPUT.PUT_LINE('ID: ' || customer.ID || ', NAME: ' || customer.NAME || ', AGE: ' || customer.AGE || ', ADDRESS: ' || customer.ADDRESS || ', SALARY: ' || customer.SALARY);
+  END LOOP;
+END;
+/
